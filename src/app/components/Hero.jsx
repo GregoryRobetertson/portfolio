5bec39dcd489730d184ef2fe608cf028e76794c4
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-01 19:14:51 -0500

    add image to hero

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <section className="">
+      <div>
+        <h1>Hi I'm Gregory Robertson</h1>
+        <p>I specialize in building websites and web application.</p>
+      </div>
+      <div>
+        <Image
+          src="/images/profile.png"
+          alt="profile image"
+          width={300}
+          height={300}
+        />
+      </div>
+    </section>
+  );
 }
