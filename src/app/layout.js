c6c592c4bb39d2b453b1011bee9c82c1b1860609
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-02 19:14:03 -0500

    changed background color to white and text color to black

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-white text-black">{children}</body>
     </html>
   );
 }
