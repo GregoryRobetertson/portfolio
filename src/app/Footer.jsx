9110531d464b94503386d8acf58237775e20b6c6
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-13 11:22:46 -0500

    created

---
+++ src/app/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="mt-8 p-2 bg-black">
+      <p className="text-center mt-4">
+        Designed and Built by Gregory Robertson
+      </p>
+    </div>
+  );
+}
