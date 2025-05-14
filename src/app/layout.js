6e7513bacaafe625bbbc146a2871df75c0ecdff7
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-14 11:26:28 -0500

    changed page bg color

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className="bg-gradient-to-r from-[#7F1D1D] to-[#1F0A0A] text-white">
+      <body className="bg-gradient-to-r from-[#1c2431] to-[#0c0f14] text-white">
         {children}
       </body>
     </html>
