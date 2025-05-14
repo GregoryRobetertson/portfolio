dd968c56c0e031532d7fa8b4de180afd2e1f8f7f
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-14 11:24:10 -0500

    changed background to an image and change the get in touch cta to blue

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-50 bg-black text-white shadow-md">
+    <div className="sticky top-0 z-50 text-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Logo */}
         <a href="/" className="text-2xl font-bold tracking-wide ml-2">
-          <span className="text-red-700 font-extrabold">G</span>regory
+          <span className="text-blue-700 font-extrabold">G</span>regory
         </a>
 
         {/* Navigation */}
