7b2304a487a58bcdf3ccd122dca6311a07e1b291
GregoryRobetertson <gregoryrobertson.dev@gmail.com>
2025-05-16 14:30:58 -0500

    removed NowStatus.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,8 @@
 
 import About from "./components/About";
 import Contact from "./components/Contact";
-
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import NowStatus from "./components/NowStatus";
 import ProjectCards from "./components/ProjectCards";
 import ServiceSection from "./components/ServiceSection";
 import Footer from "./Footer";
